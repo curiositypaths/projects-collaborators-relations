0dcbe81c7930c48508376cfb2c153b991ea8c24e
curiositypaths <curiositypaths@users.noreply.github.com>
2017-09-02 08:52:54 -0400

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  has_many :projects
+  has_many :collaborators, foreign_key: 'collaborator_id'
+  has_many :participating_projects, through: :collaborators, source: :project
+end
